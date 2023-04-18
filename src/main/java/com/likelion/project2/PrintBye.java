c9b40b8d0570ebcb73479944247029c0026027a6
Dothur <craz1000@naver.com>
2023-04-18 15:27:11 +0900

    first java project

---
+++ src/main/java/com/likelion/project2/PrintBye.java
@@ -0,0 +1,7 @@
+package com.likelion.project2;
+
+public class PrintBye {
+    public void print(){
+        System.out.println("Bye");
+    }
+}
---
+++ src/main/java/com/likelion/project2/PrintHelloTest.java
@@ -0,0 +1,10 @@
+package com.likelion.project2;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        PrintBye printBye = new PrintBye();
+        printHello.print();
+        printBye.print();
+    }
+}
